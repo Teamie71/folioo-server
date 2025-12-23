b102d4727ba0a133e406ccc5257352b74f3de559
hyoinkang <hyoin1704@gmail.com>
2025-12-24 02:48:21 +0900

    chore: TypeORM 설정 with PostgreSQL
    - 의존성 추가 @nestjs/typeorm typeorm pg @nestjs/config
    - TypeOrmModule과 ConfigService 설정

---
+++ src/config/typeorm-config.ts
@@ -0,0 +1,22 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from "@nestjs/typeorm";
+
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory{
+    constructor(private readonly configService: ConfigService){}
+
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+        return{
+            type: 'postgres',
+            host: this.configService.get('DB_HOST'),
+            port: +this.configService.get('DB_PORT'),
+            username: this.configService.get('DB_USERNAME'),
+            password: this.configService.get('DB_PASSWORD'),
+            database: this.configService.get('DB_SCHEMA'),
+            entities: [__dirname + '/../modules/**/*.entity.{ts,js}'],
+            synchronize: true,
+            logging: true,
+        }
+    }
+}
