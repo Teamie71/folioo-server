4eb3d7903da379aa01340609cf3163f78e0d5c98
강효인 <hyoin1704@gmail.com>
2025-12-25 20:26:20 +0900

    Feat: 초기 ERD 구성 (#6)
    * fix: 이슈 템플릿 네이밍 오류 해결
    
    이슈 템플릿 디렉토리가 언더 스코어로 되어 있지 않아 템플릿 적용이 안 되던 부분 해결
    DS_store 관련 githignore도 추가함
    
    * chore: TypeORM 설정 with PostgreSQL
    
    - 의존성 추가 @nestjs/typeorm typeorm pg @nestjs/config
    - TypeOrmModule과 ConfigService 설정
    
    * feat(domain): 초기 ERD 스키마 설정
    
    - abstract class BaseEntity 생성
    - 각 도메인 별 enum 생성
        (LoginType, SourceType, CorrectionStatus, InsightCategory, JobCategory, ChatStatus)
        추후 기획 사항이 확정됨에 따라 수정될 수 있음
    - 엔티티 정의 및 연관관계 설정
    - swc 사용을 위해 엔티티 간 연관관계를 단방향으로 유지함
    
    * fix: apply Copilot Review Feedback
    
    - import path의 절대경로를 상대경로로 변경
    - typeormconfig의 synchronize 및 logging 설정을 profile로 분기 처리
    - 파일명 및 칼럼명의 오타 수정
    - loginId의 nullable 설정 삭제, 관련 unique 설정은 도메인 로직으로 추가 필요

---
+++ src/modules/portfolio-correction/domain/portfolio-correction.entity.ts
@@ -0,0 +1,65 @@
+import { BaseEntity } from '../../../common/entities/base.entity';
+import { Column, Entity, ManyToOne } from 'typeorm';
+import { CorrectionStatus } from './enums/correction-status.enum';
+import { Portfolio } from '../../portfolio/domain/portfolio.entity';
+
+@Entity()
+export class PortfolioCorrection extends BaseEntity {
+    @Column({ length: 20 })
+    title: string;
+
+    @Column({ length: 20 })
+    companyName: string;
+
+    @Column({ length: 20 })
+    positionName: string;
+
+    @Column({ length: 700 })
+    jobDescription: string;
+
+    @Column({ length: 1500, nullable: true })
+    companyInsight: string;
+
+    @Column({ length: 200, nullable: true })
+    highlightPoint: string;
+
+    @Column({
+        type: 'enum',
+        enum: CorrectionStatus,
+        default: CorrectionStatus.NOT_STARTED,
+    })
+    status: CorrectionStatus;
+
+    @Column({
+        type: 'jsonb',
+        nullable: true,
+    })
+    description: Record<string, any>;
+
+    @Column({
+        type: 'jsonb',
+        nullable: true,
+    })
+    responsibilities: Record<string, any>;
+
+    @Column({
+        type: 'jsonb',
+        nullable: true,
+    })
+    problemSolving: Record<string, any>;
+
+    @Column({
+        type: 'jsonb',
+        nullable: true,
+    })
+    learnings: Record<string, any>;
+
+    @Column({
+        type: 'jsonb',
+        nullable: true,
+    })
+    overallReview: Record<string, any>;
+
+    @ManyToOne(() => Portfolio, { onDelete: 'CASCADE' })
+    portfolio: Portfolio;
+}
