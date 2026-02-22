fb7693349e79695a4b23140efb70d686ce7474e5
김덕환 <sachi009955@gmail.com>
2026-02-22 16:11:41 +0900

    fix: localhost 3000 CORS 허용 추가 (#152) (#153)

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,12 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
     initializeTransactionalContext();
     const app = await NestFactory.create(AppModule);
+
+    app.enableCors({
+        origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+        credentials: true,
+    });
+
     app.use(cookieParser());
 
     app.useGlobalPipes(
