130120c2a9005e84a55151ee40fe6f1ea872606c
IISweetHeartII <sachi009955@gmail.com>
2026-03-08 02:26:37 +0900

    refactor: 헬스체크 성공 응답 문서화 정리 (#274)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
-import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiCommonMessageResponse } from './common/decorators/swagger.decorator';
 import { Public } from './common/decorators/public.decorator';
 
 @ApiTags('Common')
@@ -10,16 +11,7 @@ export class AppController {
         description:
             '로드밸런서가 서버 상태를 확인할 때 사용하는 헬스체크용 API입니다. 로그인 없이도 테스트 가능합니다.',
     })
-    @ApiOkResponse({
-        schema: {
-            example: {
-                timestamp: '2026-01-02T14:56:23.295Z',
-                isSuccess: true,
-                error: null,
-                result: "I'm Healthy",
-            },
-        },
-    })
+    @ApiCommonMessageResponse("I'm Healthy")
     @Public()
     @Get('health')
     getHello(): string {
