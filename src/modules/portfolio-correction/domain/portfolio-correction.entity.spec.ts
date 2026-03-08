ed0431536f242e89d8df4e9b1f1f5f4d401e2583
IISweetHeartII <sachi009955@gmail.com>
2026-03-08 13:52:11 +0900

    test: 첨삭 테스트 설명 언어 일관성 수정 (#282)

--- src/modules/portfolio-correction/domain/portfolio-correction.entity.spec.ts
+++ src/modules/portfolio-correction/domain/portfolio-correction.entity.spec.ts
@@ -2,7 +2,7 @@ import { PortfolioCorrection } from './portfolio-correction.entity';
 import { JobDescriptionType } from './enums/jobdescription-type.enum';
 
 describe('PortfolioCorrection', () => {
-    it('stores the request title as-is', () => {
+    it('요청된 제목을 그대로 저장한다', () => {
         const correction = PortfolioCorrection.create(
             1,
             'Backend Resume',
