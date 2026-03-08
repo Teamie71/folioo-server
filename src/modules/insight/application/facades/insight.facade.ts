434d3167ccc05b709a5da0721aa8968d88af3c81
IISweetHeartII <sachi009955@gmail.com>
2026-03-08 17:41:08 +0900

    Refactor: Event 보상 호출 경로 의존성 정리 (#288)

--- src/modules/insight/application/facades/insight.facade.ts
+++ src/modules/insight/application/facades/insight.facade.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateInsightLogReqDTO, InsightLogResDTO } from '../dtos/insight-log.dto';
 import { InsightService } from '../services/insight.service';
-import { EventRewardFacade } from 'src/modules/event/application/facades/event-reward.facade';
+import { EventRewardLifecycleService } from 'src/modules/event/application/services/event-reward-lifecycle.service';
 import { Transactional } from 'typeorm-transactional';
 
 @Injectable()
 export class InsightFacade {
     constructor(
         private readonly insightService: InsightService,
-        private readonly eventRewardFacade: EventRewardFacade
+        private readonly eventRewardLifecycleService: EventRewardLifecycleService
     ) {}
 
     @Transactional()
     async createInsight(userId: number, body: CreateInsightLogReqDTO): Promise<InsightLogResDTO> {
         const insight = await this.insightService.createInsight(userId, body);
-        await this.eventRewardFacade.trackInsightChallengeProgress(userId);
+        await this.eventRewardLifecycleService.trackInsightChallengeProgress(userId);
         return insight;
     }
 }
