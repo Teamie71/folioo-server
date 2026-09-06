235bbbe3482ddb946ef911e1febb567634610a16
hyoinkang <hyoin1704@gmail.com>
2026-09-07 00:04:17 +0900

    fix: 체리픽 과정에서 잘못 포함된 VisualizationModule 참조 제거 (#419)
    경험 정리 v3 블록 스키마 커밋(9496cf0)의 diff에는 BlockModule만 추가되었으나,
    merge-base 컨텍스트 불일치로 인해 이미 dev에 존재하던 VisualizationModule
    import/등록까지 함께 반영되었음. 해당 모듈은 이번 릴리즈에 포함되지 않은
    별도 기능이라 참조를 제거.

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,6 @@ import { RedisModule } from './common/redis';
 import { InternalModule } from './modules/internal/internal.module';
 import { AdminModule } from './modules/admin/admin.module';
 import { FeedbackModule } from './modules/feedback/feedback.module';
-import { VisualizationModule } from './modules/visualization/visualization.module';
 import { BlockModule } from './modules/block/block.module';
 import { addTransactionalDataSource, getDataSourceByName } from 'typeorm-transactional';
 import { DataSource } from 'typeorm';
@@ -57,7 +56,6 @@ import { DataSource } from 'typeorm';
         PaymentModule,
         EventModule,
         FeedbackModule,
-        VisualizationModule,
         BlockModule,
         InterviewModule,
         InternalModule,
